df71b4f1205bf86ff83d718c3d6c61acec105513
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2023-01-20 11:23:24 -0300

    Ajustar versao

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.0.0
+ * Version:         1.0.1
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
