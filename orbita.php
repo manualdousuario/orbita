e3303ec14ebca0a000f70ff717e0926c12b4a5ec
Renan Bernordi <altendorfme@gmail.com>
2023-08-27 19:33:58 -0300

    atualizacao de versão

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.5
+ * Version:         1.5.1
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define plugin version constant
  */
-define( 'ORBITA_VERSION', '1.5' );
+define( 'ORBITA_VERSION', '1.5.1' );
 
 /**
  * Enqueue style file
