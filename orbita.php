b9cdb0cdc7d1c6e2a3ee59d71f3e5bc1e310977e
Renan Bernordi <altendorfme@gmail.com>
2023-09-16 10:59:23 -0300

    ajuste do icone de votação em darkmode

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.7.1
+ * Version:         1.7.2
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define plugin version constant
  */
-define( 'ORBITA_VERSION', '1.7.1' );
+define( 'ORBITA_VERSION', '1.7.2' );
 
 /**
  * Enqueue style file
