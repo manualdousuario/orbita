94218680e22b87c6f18fb44c92f6039c3e6d6c0f
Rodrigo Ghedin <rodrigo@ghed.in>
2023-10-27 18:45:01 -0300

    Adapta as variáveis do CSS à nova versão do tema dez, do Manual do Usuário, e faz alguns ajustes de acessibilidade

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.8.4
+ * Version:         1.8.5
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define plugin version constant
  */
-define( 'ORBITA_VERSION', '1.8.4' );
+define( 'ORBITA_VERSION', '1.8.5' );
 
 /**
  * Enqueue style file
