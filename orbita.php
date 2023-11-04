5b4ab602dccb0e3041135311f3c58200c33036ac
Rodrigo Ghedin <rodrigo@ghed.in>
2023-11-04 09:25:20 -0300

    Corrige orbita-header em telas pequenas e destaca botão “Postar”

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.8.6
+ * Version:         1.8.7
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define plugin version constant
  */
-define( 'ORBITA_VERSION', '1.8.6' );
+define( 'ORBITA_VERSION', '1.8.7' );
 
 /**
  * Enqueue style file
