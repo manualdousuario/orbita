aa285ebcea30a65d90daa52aa5c11e3119733bd3
Claromes <claromes@hey.com>
2024-07-11 20:22:06 -0300

    adiciona title

--- orbita.php
+++ orbita.php
@@ -269,9 +269,9 @@ function orbita_get_header_html() {
 	$html  = '<div class="orbita-header">';
 	$html .= '  <a href="/orbita/postar/" class="post">Postar</a>';
 	$html .= '  <div>';
-	$html .= '    <a href="/orbita/" class="option ' . ( is_page('orbita') ? 'selected' : '' ) . '">Populares</a>';
+	$html .= '    <a href="/orbita/" class="option ' . ( is_page('orbita') ? 'selected' : '' ) . '" title="As conversas recentes com mais votos e comentários.">Populares</a>';
 	$html .= '    <span>&middot;</span>';
-	$html .= '    <a href="/orbita/tudo/" class="option ' . ( is_page('tudo') ? 'selected' : '' ) . '">Tudo</a>';
+	$html .= '    <a href="/orbita/tudo/" class="option ' . ( is_page('tudo') ? 'selected' : '' ) . '" title="Todas as conversas e links em ordem cronológica inversa.">Tudo</a>';
 	$html .= '  </div>';
 	$html .= '</div>';
 	$html .= '<p class="orbita-follow">';
