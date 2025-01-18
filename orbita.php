a7a7a0087be651724fb599d87243a88a9c86a8f8
Tiago Celestino <tcelestino@gmail.com>
2025-01-18 12:18:47 -0300

    chore: patch version

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.15.2.2
+ * Version:         1.15.3.2
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
@@ -41,7 +41,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Define plugin version constant
  */
 
-define( 'ORBITA_VERSION', '1.15.2.2' );
+define( 'ORBITA_VERSION', '1.15.3.2' );
 define( 'ORBITA_IMAGE_MAX_SIZE', '10' ); // MB
 
 /**
