b4bd63259b90eab99674e269e3593d57d3899a4b
Rodrigo Ghedin <rodrigo@ghed.in>
2026-01-31 09:28:31 -0300

    Altera a versão para 2.0

--- orbita.php
+++ orbita.php
@@ -11,7 +11,7 @@
  * Plugin Name:     Órbita
  * Plugin URI:      https://gnun.es
  * Description:     Órbita é o plugin para criar um sistema Hacker News-like para o Manual do Usuário
- * Version:         1.16.4
+ * Version:         2.0
  * Author:          Gabriel Nunes
  * Author URI:      https://gnun.es
  * License:         GPL v3
@@ -41,7 +41,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Define plugin version constant
  */
 
-define( 'ORBITA_VERSION', '1.16.4' );
+define( 'ORBITA_VERSION', '2.0' );
 define( 'ORBITA_IMAGE_MAX_SIZE', '10' ); // MB
 
 /**
